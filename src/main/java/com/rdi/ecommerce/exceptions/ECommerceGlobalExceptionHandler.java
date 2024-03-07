48524aa5016577eeed6026618c71d878901cd9de
rdayok <r.dayok@native.semicolon.africa>
2024-03-07 10:11:03 +0100

    implemented the global exception handler for the ecommerce app

---
+++ src/main/java/com/rdi/ecommerce/exceptions/ECommerceGlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.rdi.ecommerce.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ECommerceGlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleInvalidArgumentsFiledExceptions(MethodArgumentNotValidException exception) {
+        Map<String, String> errorsMapped = new HashMap<>();
+        exception.getBindingResult()
+                .getFieldErrors()
+                .forEach(error -> errorsMapped.put(error.getField(), error.getDefaultMessage()));
+        return errorsMapped;
+    }
+
+    @ExceptionHandler(ECommerceException.class)
+    public ResponseEntity<Map<String, String>> handleECommerceException(ECommerceException exception) {
+        Map<String, String> error = new HashMap<>();
+        error.put("Error ", exception.getMessage());
+        return ResponseEntity.status(exception.getHttpStatus()).body(error);
+    }
+}
